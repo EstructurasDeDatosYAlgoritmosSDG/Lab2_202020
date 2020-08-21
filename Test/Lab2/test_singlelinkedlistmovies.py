61b1c5ae9d4a6f84b8239fa9576ec38e0aa15c7c
Sofía Duque Gómez <s.duqueg@uniandes.edu.co>
2020-08-20 22:41:34 -0500

    tests

---
+++ Test/Lab2/test_singlelinkedlistmovies.py
@@ -0,0 +1,25 @@
+import pytest
+import csv
+from DataStructures import listiterator as it
+from DataStructures import liststructure as lt
+
+def test_carga():
+    lst = lt.newList()
+
+    file = 'Data/themoviesdb/MoviesCastingRaw-small.csv'
+    sep = ';'
+    dialect = csv.excel()
+    dialect.delimiter = sep
+
+    assert (lt.size(lst)==0), "La lista no empieza en cero"
+
+    try:
+        with open(file,encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile,dialect=dialect)
+
+            for row in reader:
+                lt.addLast(lst,row)
+    except:
+        assert False, "Se presento un error al cargar el archivo"
+        
+
